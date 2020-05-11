f7a3dd7c9d3c709613a06c401c54baababdffb2d
DArt <qub>
2020-05-11 17:51:43 +0300

    vol.4 css modules part 2

---
+++ src/components/Profile/MyPosts/MyPosts.module.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from './MyPosts.module.css'
+
+const MyPosts = ()=>{
+    return(
+        <div className="posts">
+            
+        </div>
+    )
+}
+
+export default MyPosts
