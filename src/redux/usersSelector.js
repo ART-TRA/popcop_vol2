e22ce23d202144826a660cefe37ec1fc16ad7d96
DArt <qub>
2020-05-29 13:51:34 +0300

    vol.38 reselect

--- src/redux/usersSelector.js
+++ src/redux/usersSelector.js
@@ -1,7 +1,22 @@
+import {createSelector} from "reselect";
 
-export const getAllUsers = state => {
+//простой селектор (примитивный) принимающий весь state и возвращающий часть state (в данном случае users) и не меняющий их
+export const getUsersSimple = state => {
     return state.usersPage.users
 };
+
+//более сложный селектор выполняющий фильтрацию users (в данном случае пропускает всех)
+//задействует результат работы getAllUsers
+//т.к. происх фильтрация, то проих изм-е state и созд-ся копия массива users, а значит будет всегда выполняться перерисовка (render)
+export const getUsersSelector = state => {
+  return getUsersSimple(state).filter(u => true);
+};
+
+//сложный селектор, созд с помощью reselect, принимающий в кач-ве параметров зависимости для перерисовки
+//если завис-ти не изм-ся то перерис-ки не происходит (в завис-тях может быть неск-ко селекторов)
+export const getAllUsers = createSelector(getUsersSimple,(users) =>{
+    return users.filter(u => true); //фейковая фильтрация на время!!!
+});
 
 export const getPageSize = state => {
     return state.usersPage.pageSize
