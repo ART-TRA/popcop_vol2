466ab0a88f482c65d9f6cf6c40e143990d6ecdd3
DArt <qubon50r@mail.ru>
2020-06-04 20:15:22 +0300

    Updates

---
+++ precache-manifest.0191b152bb5ed699730be74f011720e9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b909521e3d314160ccc8a072fcb4eb22",
+    "url": "/popcop_vol2/index.html"
+  },
+  {
+    "revision": "d673bbf0ac47cd9d7df2",
+    "url": "/popcop_vol2/static/css/3.d03aebc9.chunk.css"
+  },
+  {
+    "revision": "2d2b316f84e744886e31",
+    "url": "/popcop_vol2/static/css/4.366cbcd6.chunk.css"
+  },
+  {
+    "revision": "60ee4596a5166d48c8e6",
+    "url": "/popcop_vol2/static/css/main.2d0e0d76.chunk.css"
+  },
+  {
+    "revision": "1470063dcc05ffc01014",
+    "url": "/popcop_vol2/static/js/2.bd05c547.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/popcop_vol2/static/js/2.bd05c547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d673bbf0ac47cd9d7df2",
+    "url": "/popcop_vol2/static/js/3.8a0faf93.chunk.js"
+  },
+  {
+    "revision": "2d2b316f84e744886e31",
+    "url": "/popcop_vol2/static/js/4.bc062122.chunk.js"
+  },
+  {
+    "revision": "60ee4596a5166d48c8e6",
+    "url": "/popcop_vol2/static/js/main.471aca05.chunk.js"
+  },
+  {
+    "revision": "67b585f68ce2e05f1027",
+    "url": "/popcop_vol2/static/js/runtime-main.fe189167.js"
+  },
+  {
+    "revision": "4d0b1dcbc62e9187ecb8f39e9569a569",
+    "url": "/popcop_vol2/static/media/arrow_down.4d0b1dcb.png"
+  },
+  {
+    "revision": "a4fc82dc6da3e404e72b26240c9760f1",
+    "url": "/popcop_vol2/static/media/arrow_up.a4fc82dc.png"
+  },
+  {
+    "revision": "69b17287005094f6c7fff6b71b020a79",
+    "url": "/popcop_vol2/static/media/empty_logo2.69b17287.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/webPopcop/precache-manifest.1bc0dd3b74eceffcc356178a8af7ed70.js"
+  "/popcop_vol2/precache-manifest.0191b152bb5ed699730be74f011720e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webPopcop/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/popcop_vol2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
