9dbf08339e7d8ec868d7febf6b2f7a0e650b655c
DArt <qubon50r@mail.ru>
2020-06-04 18:58:39 +0300

    Updates

---
+++ precache-manifest.1bc0dd3b74eceffcc356178a8af7ed70.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a673125f5c9769ccb42e76d828be6cb9",
+    "url": "/webPopcop/index.html"
+  },
+  {
+    "revision": "d673bbf0ac47cd9d7df2",
+    "url": "/webPopcop/static/css/3.d03aebc9.chunk.css"
+  },
+  {
+    "revision": "2d2b316f84e744886e31",
+    "url": "/webPopcop/static/css/4.366cbcd6.chunk.css"
+  },
+  {
+    "revision": "c2789a4a22e2b8fbf28b",
+    "url": "/webPopcop/static/css/main.2d0e0d76.chunk.css"
+  },
+  {
+    "revision": "1470063dcc05ffc01014",
+    "url": "/webPopcop/static/js/2.bd05c547.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/webPopcop/static/js/2.bd05c547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d673bbf0ac47cd9d7df2",
+    "url": "/webPopcop/static/js/3.8a0faf93.chunk.js"
+  },
+  {
+    "revision": "2d2b316f84e744886e31",
+    "url": "/webPopcop/static/js/4.bc062122.chunk.js"
+  },
+  {
+    "revision": "c2789a4a22e2b8fbf28b",
+    "url": "/webPopcop/static/js/main.857ac95f.chunk.js"
+  },
+  {
+    "revision": "91daaa160f6b20ac19a6",
+    "url": "/webPopcop/static/js/runtime-main.e3913eeb.js"
+  },
+  {
+    "revision": "4d0b1dcbc62e9187ecb8f39e9569a569",
+    "url": "/webPopcop/static/media/arrow_down.4d0b1dcb.png"
+  },
+  {
+    "revision": "a4fc82dc6da3e404e72b26240c9760f1",
+    "url": "/webPopcop/static/media/arrow_up.a4fc82dc.png"
+  },
+  {
+    "revision": "69b17287005094f6c7fff6b71b020a79",
+    "url": "/webPopcop/static/media/empty_logo2.69b17287.jpg"
+  }
+]);
